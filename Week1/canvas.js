a35052dd1b1e6f59bdf9cf408f50aecf360a9b89
Andy Lai <andy04lai@gmail.com>
2024-06-12 14:19:07 +0930

    Drawing Lines

--- Week1/canvas.js
+++ Week1/canvas.js
@@ -6,6 +6,30 @@ window.addEventListener("load", () => {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
 
-  ctx.strokeStyle = "red";
-  ctx.strokeRect(100, 100, 200, 500);
+  //variables
+  let painting = false;
+
+  function startPosition(e) {
+    painting = true;
+    draw(e);
+  }
+  function finishedPosition() {
+    painting = false;
+    ctx.beginPath();
+  }
+
+  function draw(e) {
+    if (!painting) return;
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+  }
+  //EventListeners
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishedPosition);
+  canvas.addEventListener("mousemove", draw);
 });
