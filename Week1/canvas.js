957884c2afb9548545b0c781bd4063d6f4b80fe3
Andy Lai <andy04lai@gmail.com>
2024-06-12 14:12:22 +0930

    Playing with Lines and Paths

---
+++ Week1/canvas.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", () => {
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
+
+  //Resizing
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
+  ctx.beginPath();
+  ctx.moveTo(100, 100);
+  ctx.lineTo(200, 100);
+  ctx.lineTo(200, 150);
+  ctx.closePath();
+  ctx.stroke();
+});
